c466b75b86226c443221802ebfabb5f731b0c393
MOHD SABIR <sabiransari01981@gmail.com>
2024-08-10 16:02:44 +0530

    sculpture

---
+++ 02sculptureList/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Sculpture from './Sculpture'
+
+
+
+function App() {
+  return (
+   <> 
+   <Sculpture></Sculpture>
+    </>
+  )
+}
+
+export default App
+
