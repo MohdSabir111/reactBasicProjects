e268f8263d879e119678a8c248f7638deb7def9c
MOHD SABIR <sabiransari01981@gmail.com>
2024-08-13 12:51:33 +0530

    website completed

--- 04React-router-dom/src/App.jsx
+++ 04React-router-dom/src/App.jsx
@@ -1,9 +1,20 @@
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AppLayout from "./layouts/AppLayout"
-import Landing from "./pages/Landing";
-import Dashboard from "./pages/Dashboard";
+import { createBrowserRouter, RouterProvider, useParams } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import Landing from "./pages/Home";
+import Dashboard from "./pages/Contact";
 import About from "./pages/About";
+import Github from "./pages/Github";
+import Contact from "./pages/Contact";
+import { Heading1 } from "lucide-react";
+
+const User = () => {
+  const { userId } = useParams(); 
+  return (
+  <>
+   <h1 className="text-center" >User ID: {userId}</h1>
+  </>
+  )
+};
 
 const router = createBrowserRouter([
   {
@@ -14,17 +25,24 @@ const router = createBrowserRouter([
         element: <Landing />,
       },
       {
-        path: "/dashboard",
-        element: 
-            <Dashboard/>
+        path: "/contact",
+        element: <Contact/>
       },
       {
         path: "/about",
         element: <About />,
       },
       {
+        path: "/github",
+        element: <Github/>,
+      },
+      {
+        path: "/user/:userId",
+        element: <User />, // the User component here
+      },
+      {
         path: "*",
-        element: <h1>PAGE NOT FOUND </h1> ,
+        element: <h1>PAGE NOT FOUND</h1>,
       }
     ],
   },
--- 04React-router-dom/src/layouts/AppLayout.jsx
+++ 04React-router-dom/src/layouts/AppLayout.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "@/components/Footer";
 
 function AppLayout() {
   return (
     <div>
-      <main className="min-h-screen container">
+     
         <Header />
         <Outlet />
-      </main>
-      <div className="p-10 text-center bg-gray-800 mt-10">Made by Sabir</div>
+   
+      <Footer/>
     </div>
   );
 }
